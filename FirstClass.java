59d2f90d5c6cee5d8c68f5fe5d844012be331e4f
amit4alljava <amit4alljava@gmail.com>
2016-09-17 17:29:13 +0530

    Codes

---
+++ FirstClass.java
@@ -0,0 +1,30 @@
+// package = folder
+// public = it provide the access outside the package
+// static - it create only one copy in the memory
+// void - no return
+// main - it is a method(function) in java
+// String - It is a Predefine class in Java
+// and it is a replacement of char[]
+// args - it is a varible name
+// [] = array
+// String args[] = it is used to take input from
+// the user from command line
+class T
+{
+   public static void main(String arr[]){
+    System.out.println("Hello T");
+    // System - it is a predefine class
+    // out it is a object of PrintStream class
+    // and println is a function to print in a new 
+    // line
+} 
+}
+class SecondClass{
+public static void main(String arr[]){
+    System.out.println("Hello Java");
+    // System - it is a predefine class
+    // out it is a object of PrintStream class
+    // and println is a function to print in a new 
+    // line
+}
+}
