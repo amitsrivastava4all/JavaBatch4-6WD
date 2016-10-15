dc6470fd3ee19c75855ea9e4320eccaaab287d50
amit4alljava <amit4alljava@gmail.com>
2016-10-15 18:34:28 +0530

    Codes

---
+++ DigitCount.java
@@ -0,0 +1,29 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+class P
+{
+	int x;
+	long y;
+}
+public class DigitCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =1234987 ;
+		Integer y  = x;
+		System.out.println(y.toString().length());
+		System.out.println(String.valueOf(x).length());
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(y.doubleValue());
+		System.out.println(Integer.toHexString(12));
+		BigInteger b = BigInteger.ONE;
+		BigInteger b2 = BigInteger.TEN;
+		b.add(b2);
+		
+		
+		
+
+	}
+
+}
